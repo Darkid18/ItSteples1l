54dc6c1463150d7a9136ad85b6308545a3feae19
YurchukIllya <mr1creper@gmail.com>
2024-12-08 16:58:00 +0200

    ...

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+#
+# def print_hi(name):
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')\
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+
+
+# Lesson 3 Sims
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
+    def add_passenger(self, *args):
+        for passenger in args:
+            self.passengers.append(passenger)
+
+    def print_passengers_name(self):
+        if self.passengers != []:
+            print(f"Name of {self.brand} passengers:")
+            for passenger in self.passengers:
+                print(passenger.name)
+
+        else:
+            print(f"There are no passengers in {self.brand}")
+
+nick = Human("Nick")
+kate = Human("Kate")
+Kira = Human("Kira")
+Hakita = Human("Hakita")
+
+car = Auto("Mercedes")
+
+car.add_passenger(nick, kate, kira, Hakita)
+
+
+car.print_passengers_name()
---
+++ sims.py
@@ -0,0 +1,50 @@
+import random
+
+class Human:
+    def __init__(self, name="Human",job = none,home = none, car = none):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+
+        def get_home(self):
+            pass
+
+        def get_job(self):
+            pass
+
+        def get_car(self):
+            pass
+
+        def eat(self):
+            pass
+
+        def  work(self):
+            pass
+
+        def shopping(self):
+            pass
+
+        def chill(self):
+            pass
+
+        def clean_house(self):
+            pass
+
+        def to_repair(self):
+            pass
+
+brands_of_car = {
+    "BMW": {"fuel":100, "strength": 100, "consumption": 6},
+    "Lada": {"fuel":50, "strength": 40, "consumption": 10},
+    "Volvo": {"fuel":7, "strength": 150, "consumption": 8},
+    "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14}
+}
+
+class Auto:
+    def __init__(self, brand_list):
+        self.brand_list = random.choice(list(brand_list))
+        self.fuel = brand_list[self.brand]["fuel"]
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
