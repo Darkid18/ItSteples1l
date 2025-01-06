7f5705d6577947110139fcd36992b8d76c201853
YurchukIllya <mr1creper@gmail.com>
2025-01-06 21:13:46 +0200

    Added a few DZ

---
+++ lesson8.py
@@ -0,0 +1,36 @@
+# import logging
+#
+# logging.basicConfig(level=logging.DEBUG, filename='logfile.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.debug("debug")
+# logging.info("Program works very well user...")
+# logging.warning("warning")
+# logging.error("Ooops! There is an error in this program user")
+# logging.critical("critical")
+
+# try:
+#     print(10/0)
+# except Exception as e:
+#     logging.error("you cant do this...")
+#
+# if 2+2 == 4:
+#     print("2 + 2 = 4 if you don't know")
+#
+# assert 2+2 == 5, "wrong result"
+#
+# """
+#
+# >>> 2 + 2
+# 5
+# """
+#
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod()
+
+def adder(*args, **kwargs):
+    result = 0
+    for _ in args:
+        result += _
+    for _ in kwargs.values():
+        result += _
+    return result
