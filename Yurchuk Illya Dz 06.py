47c17b5e3e0ad9cbe0fba84c22f369606f89936c
YurchukIllya <mr1creper@gmail.com>
2025-01-09 18:48:38 +0200

    Інші уроки в файлі Main

--- Yurchuk Illya Dz 06.py
+++ Yurchuk Illya Dz 06.py
@@ -1,19 +1,18 @@
-# result = []
-# def divider(a, b):
-#     if a < b:
-#         raise ValueError
-#     if b > 100:
-#         raise IndexError
-#     return a/b
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+try:
+    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15 ,8 : 4}
+    for key in data:
+            res = divider(key, data[key])
+            result.append(res)
 
-# data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+except (TypeError, IndexError, ValueError, ZeroDivisionError):
+    print("Error")
 
-# for key in data:
-#     try:
-#         res = divider(key, data[key])
-#         result.append(res)
-#     except (ZeroDivisionError, ValueError, IndexError):
-#         print("... hey man WHAT THE HELL IS WRONG WITH YOU?")
 
-# print(result)
-
+print(result)
