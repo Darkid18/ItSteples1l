8e09cfec01e75fa62c787214779dca0bf1593a2f
YurchukIllya <mr1creper@gmail.com>
2025-01-09 17:19:41 +0200

    Інші уроки в файлі Main

--- Lesson 10.py
+++ Lesson 10.py
@@ -3,7 +3,8 @@ import sqlite3
 connection = sqlite3.connect("ItStep_DB.sl3")
 cur = connection.cursor()
 
-cur.execute("CREATE TABLE first_table (name TEXT);")
+# cur.execute("CREATE TABLE first_table (name TEXT);")
+cur.execute("INSERT INTO first_table (name) VALUES ('ivan')")
 print(connection)
 print(cur)
 
