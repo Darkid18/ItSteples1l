34e1dc39a978cd9c7a4debddad7daea2e566b19b
YurchukIllya <mr1creper@gmail.com>
2025-01-09 19:08:19 +0200

    Інші уроки в файлі Main

--- Yurchuk Illya dz 05.py
+++ Yurchuk Illya dz 05.py
@@ -6,5 +6,5 @@ import inspect
 
 print("Is module:   ",inspect.ismodule(colorama))
 print("Is class:    ",inspect.isclass(colorama))
-print("Addres:      ",inspect.getmodule(colorama))
+print("File Addres:      ",inspect.getmodule(colorama))
 print("Is code:     ",inspect.iscode(colorama))
