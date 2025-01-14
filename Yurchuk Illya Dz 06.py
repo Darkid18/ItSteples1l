2abd9fb37cb23c8b8c38ab190b9cd7ce7eb04ce6
YurchukIllya <mr1creper@gmail.com>
2025-01-14 16:10:34 +0200

    Інші уроки в файлі Main

--- Yurchuk Illya Dz 06.py
+++ Yurchuk Illya Dz 06.py
@@ -4,15 +4,20 @@ def divider(a, b):
         raise ValueError
     if b > 100:
         raise IndexError
+    if a != int:
+        raise IndexError
+    if b != int:
+        raise IndexError
     return a/b
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15 ,8 : 4}
+
 try:
-    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15 ,8 : 4}
     for key in data:
             res = divider(key, data[key])
             result.append(res)
 
 except (TypeError, IndexError, ValueError, ZeroDivisionError):
-    print("Error")
+    print("Error! please enter correct indexes")
 
 
 print(result)
